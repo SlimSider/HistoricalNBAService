e4fc6f1cad1dfa5c8a061329b2bb80a06aca2b77
Szamosy Koppány <shadyssider@gmail.com>
2022-03-24 00:00:33 +0100

    Fixing ErrorResource

--- src/main/java/com/kszamosy/model/resource/ErrorResource.java
+++ src/main/java/com/kszamosy/model/resource/ErrorResource.java
@@ -1,8 +1,10 @@
 package com.kszamosy.model.resource;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 
 @AllArgsConstructor
+@Data
 public class ErrorResource {
     private String message;
 }
